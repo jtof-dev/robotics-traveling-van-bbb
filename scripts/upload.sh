137cb2c20a318fdb5c4ead9e5f815a7e8798842c
Andy Babcock <andybabcock@jtof.dev>
2026-03-27 14:56:52 -0700

    feat: add upload.sh script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-if [ $(id -u) -ne 0] 
-then echo Please run this script as root or using sudo!
-	exit
-fi
-
-if [ $1 -z ] then
-	echo "Please put in the drive"
-	exit
-
-fi
-
-mkdir RPI_PICO_UPLOAD_FOLDER
-mount 
+picotool load -f -x ../build/bbb.uf2
