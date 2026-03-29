d73836481862a14150d487b7cf8a3327a62769de
Kyle Draper <kpd87@nau.edu>
2026-03-29 15:57:08 -0700

    Refactored the code to have the motor class take its own seperate cpp & hpp files. This should make the code a lot easier to modify and compile

--- src/main.cpp
+++ src/main.cpp
@@ -9,88 +9,13 @@
 #include "VL53L0X.h"
 #include "configuration.hpp"
 #include "pid.h"
+#include "motor.hpp"
 
 void core1_entry() {
   while (1) {
     tight_loop_contents();
   }
 }
-
-class MOTOR {
-	public:
-		MOTOR(int step_pin, int dir_pin) {
-			set_motor_pins(step_pin, dir_pin);
-
-		}
-
-		void set_angle(float angle) {
-
-
-			int new_angle = angle / 0.225;
-
-			if (abs(new_angle - current_angle) > 1) {
-				change_angle(new_angle - current_angle);
-				current_angle = current_angle + (new_angle - current_angle);
-
-			}
-
-
-		}
-
-		void set_motor_pins(int step_pin, int dir_pin) {
-			STEP_PIN = step_pin;
-			DIR_PIN = dir_pin;
-		}
-
-		float get_current_angle() {
-			return current_angle * angle_conversion;
-		}
-
-		void set_pulsewidth_us(uint us) {
-			pulsewidth = 1000;
-
-		}
-
-		void reset_beam_angle() {
-			// Changing the angle by 120 guarentees that the beam will be on the edge
-			change_angle(60 / angle_conversion);
-			sleep_ms(1000);
-			change_angle(-30/ angle_conversion);
-			sleep_ms(1000);
-			current_angle = 0;
-		}
-
-
-	private:
-		int STEP_PIN; 
-		int DIR_PIN;
-		int pulsewidth = 1000;
-		int current_angle = 0;
-		const float angle_conversion = 1.8/8; // 1.8 steps with 8 microsteps in between
-
-		void change_angle(int angle) {
-			int new_angle = abs(angle);
-			int counter = 0;
-
-
-			if (angle > 0) {
-				gpio_put(DIR_PIN, CLOCKWISE);
-			} else {
-				gpio_put(DIR_PIN, COUNTER_CLOCKWISE);
-			}
-			sleep_us(10);
-
-			while (counter < new_angle) {
-				gpio_put(STEP_PIN, 1);
-				sleep_us(pulsewidth / 2);
-				gpio_put(STEP_PIN, 0);
-				sleep_us(pulsewidth / 2);
-
-				counter++;
-
-			}
-		}
-};
 
 int main() {
   stdio_init_all();
---
+++ src/motor.hpp
@@ -0,0 +1,27 @@
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
+
+#include "pico/stdlib.h"
+
+class MOTOR {
+	public:
+		MOTOR(int step_pin, int dir_pin);
+
+		void set_angle(float angle);
+		void set_motor_pins(int step_pin, int dir_pin);
+		float get_current_angle();
+		void set_pulsewidth_us(uint us);
+		void reset_beam_angle();
+
+	private:
+		int STEP_PIN; 
+		int DIR_PIN;
+		int pulsewidth = 1000;
+		int current_angle = 0;
+		const float angle_conversion = 1.8/8; // 1.8 steps with 8 microsteps in between
+
+		void change_angle(int angle);
+
+};
+
+#endif // MOTOR_HPP
